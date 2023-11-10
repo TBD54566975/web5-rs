0124cde041771e7c4ef9002c3668b59b93d67ccf
Adam Mika <amika@squareup.com>
2023-11-10 11:22:54 -0700

    another save

--- crates/web5-uniffi/src/crypto/key_manager.rs
+++ crates/web5-uniffi/src/crypto/key_manager.rs
@@ -1,28 +1,18 @@
+use crate::crypto::default_key_generator::DefaultKeyGenerator;
 use crate::crypto::key_generator::KeyGenerator;
 use crate::crypto::key_store::KeyStore;
 use std::fmt::Debug;
 use std::sync::Arc;
-use crate::crypto::default_key_generator::DefaultKeyGenerator;
+
+use crate::crypto::key::{KeyAlgorithm, PrivateKey};
+
+#[derive(uniffi::Error)]
+pub struct KeyManagerError {}
 
 #[uniffi::export]
-pub struct KeyManager {
-    key_generator: Arc<dyn KeyGenerator>,
-    key_store: Arc<dyn KeyStore>,
-}
-
-#[uniffi::export]
-impl KeyManager {
-    #[uniffi::constructor]
-    pub fn new(
-        key_generator: Option<Arc<dyn KeyGenerator>>,
-        key_store: Option<Arc<dyn KeyStore>>,
-    ) -> Arc<Self> {
-        let a = key_generator.unwrap_or(DefaultKeyGenerator::new().into())
-
-        Self {
-            key_generator,
-            key_store,
-        }
-        .into()
-    }
+pub trait KeyManager: Send + Sync {
+    fn generate_private_key(
+        &self,
+        key_algorithm: KeyAlgorithm,
+    ) -> Result<Arc<PrivateKey>, KeyManagerError>;
 }
