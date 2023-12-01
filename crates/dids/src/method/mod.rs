a4e82902cafec40ee1e7e388baabff0e28a61a6e
Adam Mika <amika@squareup.com>
2023-12-01 11:33:07 -0700

    add/update comments

--- crates/dids/src/method/mod.rs
+++ crates/dids/src/method/mod.rs
@@ -18,6 +18,15 @@ pub enum DidMethodError {
 /// A trait with common behavior across all DID methods.
 #[async_trait]
 pub trait DidMethod<T: Did, CreateOptions> {
+    /// The name of the implemented DID method (e.g. `jwk`).
+    ///
+    /// This is used to identify the [`DidMethod`] responsible for creating/resolving an arbitrary
+    /// DID URI.
+    ///
+    /// # Example
+    /// If a consumer wants to resolve a DID URI of `did:jwk:12345`, the method portion of the URI
+    /// (`jwk` in this example) is compared against each [`DidMethod`]'s `NAME` constant. If a match
+    /// is found, the corresponding [`DidMethod`] is used to resolve the DID URI.
     const NAME: &'static str;
 
     /// Create a new DID instance.
@@ -26,6 +35,7 @@ pub trait DidMethod<T: Did, CreateOptions> {
         options: CreateOptions,
     ) -> Result<T, DidMethodError>;
 
-    /// Resolve a DID URI to a DID Document.
+    /// Resolve a DID URI to a [`DidResolutionResult`], as specified in
+    /// [Resolving a DID](https://w3c-ccg.github.io/did-resolution/#resolving).
     async fn resolve_uri(did_uri: &str) -> DidResolutionResult;
 }
