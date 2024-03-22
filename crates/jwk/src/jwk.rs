29462dd0dccfa9edfc6870368116075266a55c48
Kendall Weihe <kendallw@squareup.com>
2024-03-22 03:00:16 -0700

    Add JWK crate

---
+++ crates/jwk/src/jwk.rs
@@ -0,0 +1,70 @@
+use base64::{encode_config, URL_SAFE_NO_PAD};
+use serde::{Deserialize, Serialize};
+use sha2::{Digest, Sha256};
+
+#[derive(Serialize, Deserialize)]
+pub struct JWK {
+    #[serde(rename = "alg", skip_serializing_if = "Option::is_none")]
+    alg: Option<String>,
+    #[serde(rename = "kty", skip_serializing_if = "Option::is_none")]
+    kty: Option<String>,
+    #[serde(rename = "crv", skip_serializing_if = "Option::is_none")]
+    crv: Option<String>,
+    #[serde(rename = "d", skip_serializing_if = "Option::is_none")]
+    d: Option<String>,
+    #[serde(rename = "x", skip_serializing_if = "Option::is_none")]
+    x: Option<String>,
+    #[serde(rename = "y", skip_serializing_if = "Option::is_none")]
+    y: Option<String>,
+}
+
+impl JWK {
+    pub fn compute_thumbprint(&self) -> Result<String, serde_json::Error> {
+        let mut thumbprint_payload = serde_json::Map::new();
+        if let Some(crv) = &self.crv {
+            thumbprint_payload.insert("crv".to_string(), serde_json::Value::String(crv.clone()));
+        }
+        if let Some(kty) = &self.kty {
+            thumbprint_payload.insert("kty".to_string(), serde_json::Value::String(kty.clone()));
+        }
+        if let Some(x) = &self.x {
+            thumbprint_payload.insert("x".to_string(), serde_json::Value::String(x.clone()));
+        }
+        if let Some(y) = &self.y {
+            thumbprint_payload.insert("y".to_string(), serde_json::Value::String(y.clone()));
+        }
+
+        let bytes = serde_json::to_vec(&thumbprint_payload)?;
+        let digest = Sha256::digest(&bytes);
+        let thumbprint = encode_config(&digest, URL_SAFE_NO_PAD);
+
+        Ok(thumbprint)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*; // Adjust this line if your JWK struct and impl are in a different module
+
+    #[test]
+    fn test_compute_thumbprint() {
+        let jwk = JWK {
+            alg: Some("".to_string()),
+            kty: Some("EC".to_string()),
+            crv: Some("secp256k1".to_string()),
+            d: Some("".to_string()),
+            x: Some("IP76NWyz81Bk1Zfsbk_ZgTJ57nTMIGM_YKdUlAUKbeY".to_string()),
+            y: Some("UefbWznggYPo3S17R9hcW5wAmwYoyfFw9xeBbQOacaA".to_string()),
+        };
+
+        let expected_thumbprint = "bgEObpJ1QzKa0jhWUkMSQKDOSDKEmwIw77ewaYpyduk";
+        let computed_thumbprint = jwk
+            .compute_thumbprint()
+            .expect("Failed to compute thumbprint");
+
+        assert_eq!(
+            computed_thumbprint, expected_thumbprint,
+            "Computed thumbprint does not match the expected value."
+        );
+    }
+}
