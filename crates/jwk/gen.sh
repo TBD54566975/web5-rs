4d44caa27a5d3beb657565c49b570ad48a75b0d1
Kendall Weihe <kendallw@squareup.com>
2024-03-25 09:05:34 -0400

    Remove swift parts, add cp cmds to scripts

--- crates/jwk/gen.sh
+++ crates/jwk/gen.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cargo build --release
-cargo run --bin uniffi-bindgen generate --config uniffi.toml --library ../../target/release/libjwk.dylib --language kotlin --out-dir out-kt
-cargo run --bin uniffi-bindgen generate --library ../../target/release/libjwk.dylib --language swift --out-dir out-swift
+cp ../../target/release/libjwk.dylib ../../examples/kotlin/src/main/resources/natives
+cargo run --bin uniffi-bindgen generate --config uniffi.toml --library ../../target/release/libjwk.dylib --language kotlin --out-dir ../../examples/kotlin/src/main/kotlin
