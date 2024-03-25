642f677a7e4c200f7b0a42a1f665cbf3800f7d99
Kendall Weihe <kendallw@squareup.com>
2024-03-25 09:09:32 -0400

    Make cp cmd relative to home dir

--- examples/kotlin/run.sh
+++ examples/kotlin/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-sudo cp ../../target/release/libjwk.dylib /Users/kendallw/Library/Java/JavaVirtualMachines/jdk-17.0.8_7.jdk/Contents/Home/bin
+# TODO find a way to not have to do this... should be contained within the project
+sudo cp ../../target/release/libjwk.dylib ~/Library/Java/JavaVirtualMachines/jdk-17.0.8_7.jdk/Contents/Home/bin
 
 mvn clean compile
 mvn package
