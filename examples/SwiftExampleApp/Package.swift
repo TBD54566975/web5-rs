c65d2a7967bb1546394539ef93011f0859f763ef
Kendall Weihe <kendallweihe@gmail.com>
2024-03-28 12:17:03 -0400

    Move swift package file into web5-swift directory (#57)

--- examples/SwiftExampleApp/Package.swift
+++ examples/SwiftExampleApp/Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftExampleApp",
     dependencies: [
-        .package(name: "Web5", path: "../../")
+        .package(name: "Web5", path: "../../web5-swift")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
