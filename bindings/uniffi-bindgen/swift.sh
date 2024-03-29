790bc173cacbee054833afa9cd0aab8607e184d4
Kendall Weihe <kendallw@squareup.com>
2024-03-29 11:19:47 -0400

    Move UniFFI into entire repo

--- bindings/uniffi-bindgen/swift.sh
+++ bindings/uniffi-bindgen/swift.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-cargo build --release
-cargo run --bin uniffi-bindgen generate --library target/release/libjwk.dylib --language swift --out-dir target/bindgen-swift
+(cd ../../; cargo build --release)
+cargo run --bin uniffi-bindgen generate --library ../../target/release/libjwk.dylib --config uniffi.toml --language swift --out-dir target/bindgen-swift
 
 mkdir -p target/xcframework-staging
 mv target/bindgen-swift/jwk.swift ../../web5-swift/Sources/UniFFI
@@ -12,6 +12,6 @@ mv target/bindgen-swift/jwkFFI.h target/xcframework-staging/
 
 rm -rf ../../web5-swift/libweb5-rs.xcframework
 xcodebuild -create-xcframework \
-  -library target/release/libjwk.dylib \
+  -library ../../target/release/libjwk.dylib \
   -headers target/xcframework-staging \
   -output ../../web5-swift/libweb5-rs.xcframework
