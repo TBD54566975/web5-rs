545d56257748efc75f250feb6e5442cbbcedc052
Kendall Weihe <kendallw@squareup.com>
2024-04-13 10:55:39 -0400

    Add bindings/wasm-web5 [blocker by josekit not compatible with wasm]

---
+++ bindings/web5-wasm/src/lib.rs
@@ -0,0 +1,30 @@
+use keys::{
+    key::Curve,
+    key_manager::{local_key_manager::LocalKeyManager, KeyManager},
+};
+use wasm_bindgen::{prelude::wasm_bindgen, JsValue};
+
+#[wasm_bindgen]
+pub struct WasmKeyManager {
+    key_manager: LocalKeyManager,
+}
+
+#[wasm_bindgen]
+impl WasmKeyManager {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> WasmKeyManager {
+        let key_manager = LocalKeyManager::new_in_memory();
+        WasmKeyManager { key_manager }
+    }
+
+    pub fn generate_private_key(&self, curve: String) -> Result<String, JsValue> {
+        let curve = match curve.as_str() {
+            "Secp256k1" => Curve::Secp256k1,
+            "Ed25519" => Curve::Ed25519,
+            _ => return Err(JsValue::from_str("Invalid curve type")),
+        };
+        self.key_manager
+            .generate_private_key(curve)
+            .map_err(|e| JsValue::from_str(&e.to_string()))
+    }
+}
