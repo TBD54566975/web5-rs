c14b70dd0a56ae60cbbd42248fc58e1815989b5e
Kendall Weihe <kendallw@squareup.com>
2024-04-14 19:34:16 -0400

    Add wasm-pack install to build script

--- bindings/wasm/build.sh
+++ bindings/wasm/build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-# cargo install wasm-pack 
+if ! command -v wasm-pack &> /dev/null; then
+    cargo install wasm-pack
+    if [ $? -ne 0 ]; then
+        echo "Failed to install wasm-pack, exiting."
+        exit 1
+    fi
+fi
 
 wasm-pack build --target nodejs --out-dir ../js/pkg
 rm ../LICENSE
