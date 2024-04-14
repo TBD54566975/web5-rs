093cd2b0ee53c3f1cb0df3a388f8670bdde0b211
Kendall Weihe <kendallw@squareup.com>
2024-04-14 19:29:59 -0400

    Add eslint

--- bindings/js/tests/crypto.test.ts
+++ bindings/js/tests/crypto.test.ts
@@ -1,49 +1,49 @@
-import { generate_ed25519_key, generate_secp256k1_key, sign_ed25519, sign_secp256k1, verify_ed25519, verify_secp256k1 } from '../pkg';
+import { generate_ed25519_key, generate_secp256k1_key, sign_ed25519, sign_secp256k1, verify_ed25519, verify_secp256k1 } from '../pkg'
 
 describe('crypto', () => {
   test('should generate Ed25519 key', () => {
-    const key = generate_ed25519_key();
-    expect(key).toBeDefined();
-    expect(key.alg).toBe('EdDSA');
-    expect(key.kty).toBe('OKP');
-    expect(key.crv).toBe('Ed25519');
-    expect(key.d).toBeDefined();
-    expect(key.x).toBeDefined();
-    expect(key.y).toBeUndefined();
-  });
+    const key = generate_ed25519_key()
+    expect(key).toBeDefined()
+    expect(key.alg).toBe('EdDSA')
+    expect(key.kty).toBe('OKP')
+    expect(key.crv).toBe('Ed25519')
+    expect(key.d).toBeDefined()
+    expect(key.x).toBeDefined()
+    expect(key.y).toBeUndefined()
+  })
 
   test('should generate Secp256k1 key', () => {
-    const key = generate_secp256k1_key();
-    expect(key).toBeDefined();
-    expect(key.alg).toBe('ES256K');
-    expect(key.kty).toBe('EC');
-    expect(key.crv).toBe('secp256k1');
-    expect(key.d).toBeDefined();
-    expect(key.x).toBeDefined();
-    expect(key.y).toBeDefined();
-  });
+    const key = generate_secp256k1_key()
+    expect(key).toBeDefined()
+    expect(key.alg).toBe('ES256K')
+    expect(key.kty).toBe('EC')
+    expect(key.crv).toBe('secp256k1')
+    expect(key.d).toBeDefined()
+    expect(key.x).toBeDefined()
+    expect(key.y).toBeDefined()
+  })
 
   test('should sign and verify using Ed25519', () => {
-    const privateKey = generate_ed25519_key();
-    const payload = new TextEncoder().encode('hello world');
+    const privateKey = generate_ed25519_key()
+    const payload = new TextEncoder().encode('hello world')
 
-    const signature = sign_ed25519(privateKey, payload);
-    expect(signature).toBeDefined();
+    const signature = sign_ed25519(privateKey, payload)
+    expect(signature).toBeDefined()
 
-    const publicKey = privateKey.to_public();
-    const verified = verify_ed25519(publicKey, payload, signature);
-    expect(verified).toBe(undefined);
-  });
+    const publicKey = privateKey.to_public()
+    const verified = verify_ed25519(publicKey, payload, signature)
+    expect(verified).toBe(undefined)
+  })
 
   test('should sign and verify using Secp256k1', () => {
-    const privateKey = generate_secp256k1_key();
-    const payload = new TextEncoder().encode('hello world');
+    const privateKey = generate_secp256k1_key()
+    const payload = new TextEncoder().encode('hello world')
 
-    const signature = sign_secp256k1(privateKey, payload);
-    expect(signature).toBeDefined();
+    const signature = sign_secp256k1(privateKey, payload)
+    expect(signature).toBeDefined()
 
-    const publicKey = privateKey.to_public();
-    const verified = verify_secp256k1(publicKey, payload, signature);
-    expect(verified).toBe(undefined);
-  });
-});
+    const publicKey = privateKey.to_public()
+    const verified = verify_secp256k1(publicKey, payload, signature)
+    expect(verified).toBe(undefined)
+  })
+})
--- bindings/js/tests/jwk.test.ts
+++ bindings/js/tests/jwk.test.ts
@@ -1,34 +1,34 @@
-import { Jwk } from '../pkg';
+import { Jwk } from '../pkg'
 
 describe('Jwk', () => {
   test('should create a new Jwk instance', () => {
-    const alg = 'ES256';
-    const kty = 'EC';
-    const crv = 'P-256';
-    const x = 'MKBCTNIcKUSDii11ySs3526iDZ8AiTo7Tu6KPAqv7D4';
-    const y = '4Etl6SRW2YiLUrN5vfvVHuhp7x8PxltmWWlbbM4IFyM';
+    const alg = 'ES256'
+    const kty = 'EC'
+    const crv = 'P-256'
+    const x = 'MKBCTNIcKUSDii11ySs3526iDZ8AiTo7Tu6KPAqv7D4'
+    const y = '4Etl6SRW2YiLUrN5vfvVHuhp7x8PxltmWWlbbM4IFyM'
 
-    const jwk = new Jwk(alg, kty, crv, undefined, x, y);
+    const jwk = new Jwk(alg, kty, crv, undefined, x, y)
 
-    expect(jwk).toBeInstanceOf(Jwk);
-    expect(jwk.alg).toEqual(alg);
-    expect(jwk.kty).toEqual(kty);
-    expect(jwk.crv).toEqual(crv);
-    expect(jwk.x).toEqual(x);
-    expect(jwk.y).toEqual(y);
-    expect(jwk.d).toBeUndefined();
-  });
+    expect(jwk).toBeInstanceOf(Jwk)
+    expect(jwk.alg).toEqual(alg)
+    expect(jwk.kty).toEqual(kty)
+    expect(jwk.crv).toEqual(crv)
+    expect(jwk.x).toEqual(x)
+    expect(jwk.y).toEqual(y)
+    expect(jwk.d).toBeUndefined()
+  })
 
   test('should compute thumbprint', () => {
-    const alg = 'ES256';
-    const kty = 'EC';
-    const crv = 'P-256';
-    const x = 'MKBCTNIcKUSDii11ySs3526iDZ8AiTo7Tu6KPAqv7D4';
-    const y = '4Etl6SRW2YiLUrN5vfvVHuhp7x8PxltmWWlbbM4IFyM';
+    const alg = 'ES256'
+    const kty = 'EC'
+    const crv = 'P-256'
+    const x = 'MKBCTNIcKUSDii11ySs3526iDZ8AiTo7Tu6KPAqv7D4'
+    const y = '4Etl6SRW2YiLUrN5vfvVHuhp7x8PxltmWWlbbM4IFyM'
 
-    const jwk = new Jwk(alg, kty, crv, undefined, x, y);
-    const thumbprint = jwk.compute_thumbprint();
+    const jwk = new Jwk(alg, kty, crv, undefined, x, y)
+    const thumbprint = jwk.compute_thumbprint()
 
-    expect(typeof thumbprint).toBe('string');
-  });
-});
+    expect(typeof thumbprint).toBe('string')
+  })
+})
