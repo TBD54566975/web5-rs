7407520915bc8e49ee904b2637b5ecb0edba9e1d
Kendall Weihe <kendallw@squareup.com>
2024-04-16 12:40:31 -0400

    Add examples to workspace, remove uniffi config, cp instead of mv

--- bindings/uniffi/kotlin.sh
+++ bindings/uniffi/kotlin.sh
@@ -5,5 +5,5 @@ set -e
 cargo build --release
 cargo run --bin uniffi-bindgen generate --library ../../target/release/libweb5.dylib --config uniffi.toml --language kotlin --out-dir target/bindgen-kotlin
 
-mv ../../target/release/libweb5.dylib ../kt/src/main/resources/natives
-mv target/bindgen-kotlin/web5/sdk/web5.kt ../kt/src/main/kotlin/web5/sdk
+cp ../../target/release/libweb5.dylib ../kt/src/main/resources/natives
+cp target/bindgen-kotlin/web5/sdk/web5.kt ../kt/src/main/kotlin/web5/sdk
