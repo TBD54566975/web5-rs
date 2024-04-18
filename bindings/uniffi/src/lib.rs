f947d1a1822dc3f3baf2c99af70429cb551ea64a
Kendall Weihe <kendallw@squareup.com>
2024-04-17 23:16:49 -0400

    Write UDL for bearer did from key manager

--- bindings/uniffi/src/lib.rs
+++ bindings/uniffi/src/lib.rs
@@ -1,4 +1,5 @@
 use ::crypto::Curve;
+use ::dids::bearer::{BearerDid, BearerDidError};
 use ::jwk::{Jwk, JwkError};
 use ::keys::{
     key::{Key, KeyError, PrivateKey, PublicKey},
@@ -8,5 +9,14 @@ use ::keys::{
         KeyManager, KeyManagerError,
     },
 };
+use std::sync::Arc;
+
+pub async fn bearer_did_from_key_manager(
+    did_uri: &str,
+    key_manager: Arc<dyn KeyManager>,
+) -> Result<Arc<BearerDid>, BearerDidError> {
+    let bearer_did = BearerDid::from_key_manager(did_uri, key_manager).await?;
+    Ok(Arc::new(bearer_did))
+}
 
 uniffi::include_scaffolding!("web5");
