57c2a5c59a63172e405d93a22669b3a5276a1190
Kendall Weihe <kendallw@squareup.com>
2024-04-22 11:31:13 -0400

    Add Justfile, rename bindings/js to bindings/ts

--- bindings/kt/install.sh
+++
@@ -1,8 +0,0 @@
-#/bin/bash
-
-set -e
-
-# TODO find a way to not have to do this... should be contained within the project
-sudo cp src/main/resources/natives/libweb5.dylib ~/Library/Java/JavaVirtualMachines/jdk-17.0.8_7.jdk/Contents/Home/bin
-
-mvn clean install
--- bindings/uniffi/kotlin.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-cargo run --bin uniffi-bindgen generate --library ../../target/release/libweb5.dylib --language kotlin --out-dir target/bindgen-kotlin
-
-cp ../../target/release/libweb5.dylib ../kt/src/main/resources/natives
-cp target/bindgen-kotlin/web5/sdk/web5.kt ../kt/src/main/kotlin/web5/sdk
