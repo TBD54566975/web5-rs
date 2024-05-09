b6446e77dba479efee0abae75c4fc3e484aa698d
Kendall Weihe <kendallw@squareup.com>
2024-05-08 21:00:54 -0400

    Remove old jose code

--- crates/jwt/src/jws.rs
+++ crates/jwt/src/jws.rs
@@ -1,6 +1,6 @@
-use crate::lib_v2::{Claims, JwtError};
+use crate::{Claims, JwtError};
+use ::jws::{CompactJws, JwsHeader};
 use dids::{bearer::BearerDid, document::KeySelector};
-use jws::v2::{CompactJws, JwsHeader};
 
 // A JWT can be implemented as either a JWS or JWE, this module is the implementation of a JWT as a JWS
 
@@ -70,7 +70,7 @@ impl Jwt {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::lib_v2::RegisteredClaims;
+    use crate::RegisteredClaims;
     use crypto::Curve;
     use dids::{
         document::KeySelector,
