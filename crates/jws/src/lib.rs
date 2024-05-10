e0ce61c1cb83c2baec984ca07f156a409a39ab86
kirahsapong <102400653+kirahsapong@users.noreply.github.com>
2024-05-10 09:48:01 -0700

    Add JWS tests (#193)
    * add jws tests, rename supported alg error
    
    * fix bad rebase
    
    * add tests for decode and crypto errors

--- crates/jws/src/lib.rs
+++ crates/jws/src/lib.rs
@@ -19,7 +19,7 @@ pub enum JwsError {
     #[error(transparent)]
     ResolutionError(#[from] ResolutionError),
     #[error("algorithm not found {0}")]
-    AlgorithmNotFound(String),
+    AlgorithmNotSupported(String),
     #[error(transparent)]
     CryptoError(#[from] CryptoError),
     #[error("serde json error {0}")]
@@ -129,7 +129,7 @@ impl CompactJws {
         match alg.as_str() {
             "EdDSA" => Ed25519::verify(&public_key, &to_verify.into_bytes(), &decoded_signature),
             "ES256K" => Secp256k1::verify(&public_key, &to_verify.into_bytes(), &decoded_signature),
-            _ => return Err(JwsError::AlgorithmNotFound(alg)),
+            _ => return Err(JwsError::AlgorithmNotSupported(alg)),
         }?;
         Ok(jws_decoded)
     }
@@ -190,16 +190,350 @@ mod tests {
     }
 
     #[test]
-    fn test_jws_decode_error() {
+    fn test_jws_decode() {
+        let key_manager = LocalKeyManager::new_in_memory();
+        let bearer_did = DidJwk::create(
+            Arc::new(key_manager),
+            DidJwkCreateOptions {
+                curve: Curve::Ed25519,
+            },
+        )
+        .expect("failed to create bearer did");
+
+        let key_id = bearer_did.document.verification_method[0].id.clone();
+
+        let header = JwsHeader {
+            alg: "EdDSA".to_string(),
+            kid: key_id.clone(),
+            typ: "JWT".to_string(),
+        };
+        let payload = json!({
+            "sub": "1234567890",
+            "name": "John Doe",
+            "iat": 1516239022
+        })
+        .to_string()
+        .into_bytes();
+
+        let compact_jws = CompactJws::sign(
+            &bearer_did,
+            &KeySelector::KeyId {
+                key_id: key_id.clone(),
+            },
+            &header,
+            &payload,
+        )
+        .unwrap();
+
+        let decoded_jws = CompactJws::decode(&compact_jws).unwrap();
+
+        assert_eq!(decoded_jws.header, header);
+        assert_eq!(decoded_jws.payload, payload);
+        assert_eq!(decoded_jws.signature.is_empty(), false);
+        assert_eq!(decoded_jws.parts.len(), 3);
+    }
+
+    #[test]
+    fn test_jws_decode_incorrect_parts_error() {
         let invalid_jws = "invalid.jws";
         let result = CompactJws::decode(invalid_jws);
         assert!(matches!(result, Err(JwsError::IncorrectPartsLength(_))));
     }
 
-    // TODO https://github.com/TBD54566975/web5-rs/issues/166
-    // - not base64 encoded signature
-    // - base64 encoded signature but not valid cryptographic signature
-    // - not supported algorithm
-    // - did doc doesn't resolve
-    // - did doc missing vm
+    #[tokio::test]
+    async fn test_jws_verify_malformed_header_alg_error() {
+        let key_manager = LocalKeyManager::new_in_memory();
+        let bearer_did = DidJwk::create(
+            Arc::new(key_manager),
+            DidJwkCreateOptions {
+                curve: Curve::Ed25519,
+            },
+        )
+        .expect("failed to create bearer did");
+
+        let key_id = bearer_did.document.verification_method[0].id.clone();
+
+        let header = JwsHeader {
+            alg: "".to_string(),
+            kid: key_id.clone(),
+            typ: "JWT".to_string(),
+        };
+        let payload = json!({
+            "sub": "1234567890",
+            "name": "John Doe",
+            "iat": 1516239022
+        })
+        .to_string()
+        .into_bytes();
+
+        let compact_jws = CompactJws::sign(
+            &bearer_did,
+            &KeySelector::KeyId {
+                key_id: key_id.clone(),
+            },
+            &header,
+            &payload,
+        )
+        .unwrap();
+
+        let result = CompactJws::verify(&compact_jws).await;
+
+        assert!(matches!(result, Err(JwsError::MalformedHeader(_))));
+    }
+
+    #[tokio::test]
+    async fn test_jws_verify_malformed_header_kid_error() {
+        let key_manager = LocalKeyManager::new_in_memory();
+        let bearer_did = DidJwk::create(
+            Arc::new(key_manager),
+            DidJwkCreateOptions {
+                curve: Curve::Ed25519,
+            },
+        )
+        .expect("failed to create bearer did");
+
+        let key_id = bearer_did.document.verification_method[0].id.clone();
+
+        let header = JwsHeader {
+            alg: "EdDSA".to_string(),
+            kid: "".to_string(),
+            typ: "JWT".to_string(),
+        };
+        let payload = json!({
+            "sub": "1234567890",
+            "name": "John Doe",
+            "iat": 1516239022
+        })
+        .to_string()
+        .into_bytes();
+
+        let compact_jws = CompactJws::sign(
+            &bearer_did,
+            &KeySelector::KeyId {
+                key_id: key_id.clone(),
+            },
+            &header,
+            &payload,
+        )
+        .unwrap();
+
+        let result = CompactJws::verify(&compact_jws).await;
+
+        assert!(matches!(result, Err(JwsError::MalformedHeader(_))));
+    }
+
+    #[tokio::test]
+    async fn test_jws_verify_algorithm_not_supported_error() {
+        let key_manager = LocalKeyManager::new_in_memory();
+        let bearer_did = DidJwk::create(
+            Arc::new(key_manager),
+            DidJwkCreateOptions {
+                curve: Curve::Ed25519,
+            },
+        )
+        .expect("failed to create bearer did");
+
+        let key_id = bearer_did.document.verification_method[0].id.clone();
+
+        let header = JwsHeader {
+            alg: "UNSUPPORTED_ALG".to_string(),
+            kid: key_id.clone(),
+            typ: "JWT".to_string(),
+        };
+        let payload = json!({
+            "sub": "1234567890",
+            "name": "John Doe",
+            "iat": 1516239022
+        })
+        .to_string()
+        .into_bytes();
+
+        let compact_jws = CompactJws::sign(
+            &bearer_did,
+            &KeySelector::KeyId {
+                key_id: key_id.clone(),
+            },
+            &header,
+            &payload,
+        )
+        .unwrap();
+
+        let result = CompactJws::verify(&compact_jws).await;
+
+        assert!(matches!(result, Err(JwsError::AlgorithmNotSupported(_))));
+    }
+
+    #[tokio::test]
+    async fn test_jws_verify_resolution_error() {
+        let key_manager = LocalKeyManager::new_in_memory();
+        let bearer_did = DidJwk::create(
+            Arc::new(key_manager),
+            DidJwkCreateOptions {
+                curve: Curve::Ed25519,
+            },
+        )
+        .expect("failed to create bearer did");
+
+        let key_id = bearer_did.document.verification_method[0].id.clone();
+
+        let header = JwsHeader {
+            alg: "EdDSA".to_string(),
+            kid: "did:jwk:123#123".to_string(),
+            typ: "JWT".to_string(),
+        };
+        let payload = json!({
+            "sub": "1234567890",
+            "name": "John Doe",
+            "iat": 1516239022
+        })
+        .to_string()
+        .into_bytes();
+
+        let compact_jws = CompactJws::sign(
+            &bearer_did,
+            &KeySelector::KeyId {
+                key_id: key_id.clone(),
+            },
+            &header,
+            &payload,
+        )
+        .unwrap();
+
+        let result = CompactJws::verify(&compact_jws).await;
+
+        assert!(matches!(result, Err(JwsError::ResolutionError(_))));
+    }
+
+    #[tokio::test]
+    async fn test_jws_verify_document_verification_method_error() {
+        let key_manager = LocalKeyManager::new_in_memory();
+        let bearer_did = DidJwk::create(
+            Arc::new(key_manager),
+            DidJwkCreateOptions {
+                curve: Curve::Ed25519,
+            },
+        )
+        .expect("failed to create bearer did");
+
+        let key_id = bearer_did.document.verification_method[0].id.clone();
+
+        let header = JwsHeader {
+            alg: "EdDSA".to_string(),
+            kid: key_id.clone() + "123",
+            typ: "JWT".to_string(),
+        };
+        let payload = json!({
+            "sub": "1234567890",
+            "name": "John Doe",
+            "iat": 1516239022
+        })
+        .to_string()
+        .into_bytes();
+
+        let compact_jws = CompactJws::sign(
+            &bearer_did,
+            &KeySelector::KeyId {
+                key_id: key_id.clone(),
+            },
+            &header,
+            &payload,
+        )
+        .unwrap();
+
+        let result = CompactJws::verify(&compact_jws).await;
+
+        assert!(matches!(result, Err(JwsError::DocumentError(_))));
+    }
+
+    #[tokio::test]
+    async fn test_jws_verify_signature_decode_error() {
+        let key_manager = LocalKeyManager::new_in_memory();
+        let bearer_did = DidJwk::create(
+            Arc::new(key_manager),
+            DidJwkCreateOptions {
+                curve: Curve::Ed25519,
+            },
+        )
+        .expect("failed to create bearer did");
+
+        let key_id = bearer_did.document.verification_method[0].id.clone();
+
+        let header = JwsHeader {
+            alg: "EdDSA".to_string(),
+            kid: key_id.clone(),
+            typ: "JWT".to_string(),
+        };
+        let payload = json!({
+            "sub": "1234567890",
+            "name": "John Doe",
+            "iat": 1516239022
+        })
+        .to_string()
+        .into_bytes();
+
+        let compact_jws = CompactJws::sign(
+            &bearer_did,
+            &KeySelector::KeyId {
+                key_id: key_id.clone(),
+            },
+            &header,
+            &payload,
+        )
+        .unwrap() + "123";
+
+        let result = CompactJws::verify(&compact_jws).await;
+
+        assert!(matches!(result, Err(JwsError::DecodeError(_))));
+    }
+
+    #[tokio::test]
+    async fn test_jws_verify_signature_crypto_error() {
+        let key_manager = LocalKeyManager::new_in_memory();
+        let bearer_did = DidJwk::create(
+            Arc::new(key_manager),
+            DidJwkCreateOptions {
+                curve: Curve::Ed25519,
+            },
+        )
+        .expect("failed to create bearer did");
+
+        let key_id = bearer_did.document.verification_method[0].id.clone();
+
+        let header = JwsHeader {
+            alg: "EdDSA".to_string(),
+            kid: key_id.clone(),
+            typ: "JWT".to_string(),
+        };
+        let payload = json!({
+            "sub": "1234567890",
+            "name": "John Doe",
+            "iat": 1516239022
+        })
+        .to_string()
+        .into_bytes();
+
+        let invalid_key_manager = LocalKeyManager::new_in_memory();
+        let invalid_bearer_did = DidJwk::create(
+            Arc::new(invalid_key_manager),
+            DidJwkCreateOptions {
+                curve: Curve::Ed25519,
+            },
+        )
+        .expect("failed to create bearer did");
+        let invalid_key_id = invalid_bearer_did.document.verification_method[0].id.clone();
+
+        let compact_jws = CompactJws::sign(
+            &invalid_bearer_did,
+            &KeySelector::KeyId {
+                key_id: invalid_key_id.clone(),
+            },
+            &header,
+            &payload,
+        )
+        .unwrap();
+
+        let result = CompactJws::verify(&compact_jws).await;
+
+        assert!(matches!(result, Err(JwsError::CryptoError(_))));
+    }
 }
