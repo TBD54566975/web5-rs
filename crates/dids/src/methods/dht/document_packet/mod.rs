8f08e785812609663ee3c5ef8bb52a8bd1b00f39
Diane Huxley <dhuxley@tbd.email>
2024-05-22 13:10:08 -0700

    Convert did:dht services to dns records (#216)
    * Convert did:dht services to dns records
    
    * Handle url fragment using url crate
    
    * Handle multiple service endpoints
    
    * Fix after rebase
    
    * Update crates/dids/src/methods/dht/document_packet/service.rs
    
    Co-authored-by: Kendall Weihe <kendallweihe@gmail.com>
    
    * Update crates/dids/src/methods/dht/document_packet/service.rs
    
    Co-authored-by: Kendall Weihe <kendallweihe@gmail.com>
    
    * PR comments
    
    ---------
    
    Co-authored-by: Kendall Weihe <kendallweihe@gmail.com>

---
+++ crates/dids/src/methods/dht/document_packet/mod.rs
@@ -0,0 +1,19 @@
+use simple_dns::SimpleDnsError;
+
+mod rdata_encoder;
+pub mod service;
+
+const DEFAULT_TTL: u32 = 7200; // seconds
+
+/// Errors that can occur when converting between did:dht documents and DNS packets.
+#[derive(thiserror::Error, Debug)]
+pub enum DocumentPacketError {
+    #[error(transparent)]
+    Dns(#[from] SimpleDnsError),
+    #[error("Could not convert between publicKeyJwk and resource record: {0}")]
+    PublicKeyJwk(String),
+    #[error("Could not extract fragment from DID url {0}")]
+    MissingFragment(String),
+    #[error("RData was invalid: {0}")]
+    RDataError(String),
+}
