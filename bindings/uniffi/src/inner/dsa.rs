f4b6df717a3bafbe41c0d116f936ff70c2551242
Neal <groessler24@gmail.com>
2024-06-12 13:36:15 -0700

    lint1

--- bindings/uniffi/src/inner/dsa.rs
+++ bindings/uniffi/src/inner/dsa.rs
@@ -1,15 +1,15 @@
 use crate::inner::keys::Jwk;
 
 pub enum Dsa {
-  Ed25519,
+    Ed25519,
 }
 
 pub trait Signer: Send + Sync {
-  fn sign(&self, _payload: &[u8]) -> Vec<u8>;
+    fn sign(&self, _payload: &[u8]) -> Vec<u8>;
 }
 
 pub trait Verifier: Send + Sync {
-  fn verify(&self, _message: &[u8], _signature: &[u8]) -> bool;
+    fn verify(&self, _message: &[u8], _signature: &[u8]) -> bool;
 }
 
 // pub struct Ed25519Generator {}
@@ -23,31 +23,31 @@ pub trait Verifier: Send + Sync {
 pub struct Ed25519Signer {}
 
 impl Ed25519Signer {
-  pub fn new(_private_key: Jwk) -> Self {
-      println!("Invoked Ed25519Signer::new()");
-      Self {}
-  }
+    pub fn new(_private_key: Jwk) -> Self {
+        println!("Invoked Ed25519Signer::new()");
+        Self {}
+    }
 }
 
 impl Signer for Ed25519Signer {
-  fn sign(&self, _payload: &[u8]) -> Vec<u8> {
-      println!("Invoked Ed25519Signer.sign()");
-      Vec::new()
-  }
+    fn sign(&self, _payload: &[u8]) -> Vec<u8> {
+        println!("Invoked Ed25519Signer.sign()");
+        Vec::new()
+    }
 }
 
 pub struct Ed25519Verifier {}
 
 impl Ed25519Verifier {
-  pub fn new(_public_key: Jwk) -> Self {
-      println!("Invoked Ed25519Verifier::new()");
-      Self {}
-  }
+    pub fn new(_public_key: Jwk) -> Self {
+        println!("Invoked Ed25519Verifier::new()");
+        Self {}
+    }
 }
 
 impl Verifier for Ed25519Verifier {
-  fn verify(&self, _message: &[u8], _signature: &[u8]) -> bool {
-      println!("Invoked Ed25519Verifier.verify()");
-      true
-  }
+    fn verify(&self, _message: &[u8], _signature: &[u8]) -> bool {
+        println!("Invoked Ed25519Verifier.verify()");
+        true
+    }
 }
