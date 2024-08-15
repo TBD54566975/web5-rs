d7558e4f655782f2c4cd1aab88e3517535ad63a1
Diane Huxley <dhuxley@tbd.email>
2024-08-15 10:40:03 -0700

    Add debug logging

--- crates/web5/src/lib.rs
+++ crates/web5/src/lib.rs
@@ -4,3 +4,30 @@ pub mod dids;
 
 #[cfg(test)]
 mod test_helpers;
+
+lazy_static::lazy_static! {
+  pub(crate) static ref LOG_LEVEL: Option<String> = {
+      std::env::var("WEB5_SDK_LOG_LEVEL").ok()
+  };
+}
+
+pub(crate) mod logging {
+  #[macro_export]
+  macro_rules! log_dbg {
+      ($msg:expr, $($arg:tt)*) => {
+          if let Some(ref level) = *$crate::LOG_LEVEL {
+              if level == "DEBUG" {
+                  println!("[DEBUG] {}", format!($msg, $($arg)*));
+              }
+          }
+      };
+      ($closure:expr) => {
+          if let Some(ref level) = *$crate::LOG_LEVEL {
+              if level == "DEBUG" {
+                  let msg = $closure();
+                  println!("[DEBUG] {}", msg);
+              }
+          }
+      };
+  }
+}
