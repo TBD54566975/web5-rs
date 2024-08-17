e4155d758d6590705b21d6bf7053e7f1d086960d
Diane Huxley <dhuxley@tbd.email>
2024-08-16 17:11:28 -0700

    Fix and test log_dbg

--- crates/web5/src/lib.rs
+++ crates/web5/src/lib.rs
@@ -1,3 +1,5 @@
+use std::{env, sync::LazyLock};
+
 pub mod credentials;
 pub mod crypto;
 pub mod dids;
@@ -5,29 +7,44 @@ pub mod dids;
 #[cfg(test)]
 mod test_helpers;
 
-lazy_static::lazy_static! {
-  pub(crate) static ref LOG_LEVEL: Option<String> = {
-      std::env::var("WEB5_SDK_LOG_LEVEL").ok()
-  };
-}
+pub const GIT_COMMIT_HASH: &str = env!("WEB5_GIT_COMMIT_HASH");
+
+// TODO: https://github.com/TBD54566975/web5-rs/issues/287
+#[allow(dead_code)]
+static LOG_LEVEL: LazyLock<Option<String>> = LazyLock::new(|| {
+    // Default log level if the environment variable is not set
+    env::var("LOG_LEVEL").ok()
+});
 
 pub(crate) mod logging {
     #[macro_export]
     macro_rules! log_dbg {
-      ($msg:expr, $($arg:tt)*) => {
-          if let Some(ref level) = *$crate::LOG_LEVEL {
-              if level == "DEBUG" {
-                  println!("[DEBUG] {}:{}", env!("GIT_COMMIT_HASH"), format!($msg, $($arg)*));
-              }
-          }
-      };
-      ($closure:expr) => {
-          if let Some(ref level) = *$crate::LOG_LEVEL {
-              if level == "DEBUG" {
-                  let msg = $closure();
-                  println!("[DEBUG] {}:{}", env!("GIT_COMMIT_HASH"), msg);
-              }
-          }
-      };
-  }
+        ($msg:literal $(, $arg:tt)*) => {
+            if let Some(ref level) = *$crate::LOG_LEVEL {
+                if level == "DEBUG" {
+                  println!("[DEBUG] {}:{}", env!("WEB5_GIT_COMMIT_HASH"), format!($msg, $($arg)*));
+                }
+            }
+        };
+        ($closure:expr) => {
+            if let Some(ref level) = *$crate::LOG_LEVEL {
+                if level == "DEBUG" {
+                    let msg = $closure();
+                    println!("[DEBUG] {}:{}", env!("WEB5_GIT_COMMIT_HASH"), msg);
+                }
+            }
+        };
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::log_dbg;
+
+    #[test]
+    fn can_log_dbg() {
+        log_dbg!("Log debugging without arguments");
+        log_dbg!("Log debugging with arguments {}", "Some value");
+        log_dbg!(|| { 2 + 2 });
+    }
 }
