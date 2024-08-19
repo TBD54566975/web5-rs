2af9bdd40a05f3f3b241b24fc330d502f3754b50
Kendall Weihe <kendallweihe@gmail.com>
2024-08-19 16:30:03 -0400

    Add web5_proc_macros with git_sha proc macro (#290)
    * Add web5_proc_macros with git_sha proc macro
    
    * Use short SHA, case insensitive log level, remove kt code
    
    * PR review
    
    * Fix linting, fix linux builds
    
    * Fix CLI build

--- crates/web5/src/lib.rs
+++ crates/web5/src/lib.rs
@@ -9,34 +9,29 @@ pub mod json;
 pub mod rfc3339;
 
 #[cfg(test)]
-mod test_vectors;
-#[cfg(test)]
 mod test_helpers;
-
-pub const GIT_COMMIT_HASH: &str = env!("WEB5_GIT_COMMIT_HASH");
+#[cfg(test)]
+mod test_vectors;
 
 // TODO: https://github.com/TBD54566975/web5-rs/issues/287
 #[allow(dead_code)]
-static LOG_LEVEL: LazyLock<Option<String>> = LazyLock::new(|| {
-    // Default log level if the environment variable is not set
-    env::var("LOG_LEVEL").ok()
-});
+static LOG_LEVEL: LazyLock<Option<String>> = LazyLock::new(|| env::var("WEB5_SDK_LOG_LEVEL").ok());
 
 pub(crate) mod logging {
     #[macro_export]
     macro_rules! log_dbg {
         ($msg:literal $(, $arg:tt)*) => {
             if let Some(ref level) = *$crate::LOG_LEVEL {
-                if level == "DEBUG" {
-                  println!("[DEBUG] {}:{}", env!("WEB5_GIT_COMMIT_HASH"), format!($msg, $($arg)*));
+                if level.to_lowercase() == "debug" {
+                   println!("[DEBUG] {}: {}", web5_proc_macros::git_sha!(), format!($msg, $($arg)*));
                 }
             }
         };
         ($closure:expr) => {
             if let Some(ref level) = *$crate::LOG_LEVEL {
-                if level == "DEBUG" {
+                if level.to_lowercase() == "debug" {
                     let msg = $closure();
-                    println!("[DEBUG] {}:{}", env!("WEB5_GIT_COMMIT_HASH"), msg);
+                    println!("[DEBUG] {}: {}", web5_proc_macros::git_sha!(), msg);
                 }
             }
         };
