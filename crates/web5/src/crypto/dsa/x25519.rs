25448c0b795e99254f63ce7170fa1d93130909a4
Diane Huxley <dhuxley@tbd.email>
2024-08-21 10:21:30 -0700

    Generate X25519 keys

--- crates/web5/src/crypto/dsa/x25519.rs
+++ crates/web5/src/crypto/dsa/x25519.rs
@@ -1,8 +1,34 @@
 use base64::{engine::general_purpose, Engine as _};
-use ed25519_dalek::PUBLIC_KEY_LENGTH;
+use x25519_dalek::{StaticSecret, PublicKey};
 
 use super::{DsaError, Result};
 use crate::crypto::jwk::Jwk;
+
+const PUBLIC_KEY_LENGTH: usize = 32;
+
+pub struct X25519Generator;
+
+impl X25519Generator {
+    pub fn generate() -> Result<Jwk> {
+        let private_key = StaticSecret::random();
+        let public_key = PublicKey::from(&private_key);
+        let x = general_purpose::URL_SAFE_NO_PAD.encode(public_key.as_bytes());
+        let d = general_purpose::URL_SAFE_NO_PAD.encode(private_key.as_bytes());
+
+        let public_key_jwk = Jwk {
+            alg: Some("ECDH-ES+A256KW".to_string()),
+            kty: "OKP".to_string(),
+            crv: "X25519".to_string(),
+            d: Some(d),
+            x,
+            y: None,
+        };
+
+        Ok(public_key_jwk)
+    }
+}
+
+
 
 pub fn public_jwk_extract_bytes(jwk: &Jwk) -> Result<Vec<u8>> {
     let x_bytes = general_purpose::URL_SAFE_NO_PAD.decode(&jwk.x)?;
@@ -37,4 +63,50 @@ pub fn public_jwk_from_bytes(public_key_bytes: &[u8]) -> Result<Jwk> {
         x,
         y: None,
     })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    mod generate {
+        use super::*;
+
+        #[test]
+        fn test_must_set_alg() {
+            let jwk = X25519Generator::generate().unwrap();
+            assert_eq!(jwk.alg, Some("ECDH-ES+A256KW".to_string()));
+        }
+
+        #[test]
+        fn test_must_set_kty() {
+            let jwk = X25519Generator::generate().unwrap();
+            assert_eq!(jwk.kty, "OKP".to_string());
+        }
+
+        #[test]
+        fn test_must_set_crv() {
+            let jwk = X25519Generator::generate().unwrap();
+            assert_eq!(jwk.crv, "X25519");
+        }
+
+        #[test]
+        fn test_must_set_public_key_with_correct_length() {
+            let jwk = X25519Generator::generate().unwrap();
+            let public_key_bytes = general_purpose::URL_SAFE_NO_PAD
+                .decode(&jwk.x)
+                .expect("Failed to decode public key");
+            assert_eq!(public_key_bytes.len(), PUBLIC_KEY_LENGTH);
+        }
+
+        #[test]
+        fn test_must_set_private_key_with_correct_length() {
+            let jwk = X25519Generator::generate().unwrap();
+            let private_key_bytes = jwk.d.expect("Private key is missing");
+            let decoded_private_key_bytes = general_purpose::URL_SAFE_NO_PAD
+                .decode(private_key_bytes)
+                .expect("Failed to decode private key");
+            assert_eq!(decoded_private_key_bytes.len(), 32); // X25519 private key length
+        }
+    }
 }
