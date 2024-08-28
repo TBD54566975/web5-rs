7314fd47214402a70e29de34d1944dd8b197d0a0
Diane Huxley <dhuxley@tbd.email>
2024-08-27 17:08:35 -0700

    Generates bindings but linking error due to reqwest mac system config

--- bindings/web5_flutter_rust_bridge/rust/src/api/simple.rs
+++ bindings/web5_flutter_rust_bridge/rust/src/api/simple.rs
@@ -1,6 +1,16 @@
+// use web5::dids::methods::did_jwk::DidJwk;
+
+use web5::json::JsonValue;
+
 #[flutter_rust_bridge::frb(sync)] // Synchronous mode for simplicity of the demo
 pub fn greet(name: String) -> String {
     format!("Hello, {name}!")
+}
+
+#[flutter_rust_bridge::frb(sync)] // Synchronous mode for simplicity of the demo
+pub fn create_did_jwk() -> String {
+    // DidJwk::create(None).unwrap().did.uri
+    format!("{:?}", JsonValue::Null)
 }
 
 #[flutter_rust_bridge::frb(init)]
