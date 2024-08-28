00838555aa22854b40fb00c1ec5f2a310c74ae53
Kendall Weihe <kendallw@squareup.com>
2024-08-28 10:14:47 -0400

    Add sign and sign_with_did unit tests

--- crates/web5/src/credentials/verifiable_credential_1_1.rs
+++ crates/web5/src/credentials/verifiable_credential_1_1.rs
@@ -406,11 +406,7 @@ impl VerifiableCredential {
         verification_method_id: Option<String>,
     ) -> Result<String> {
         // contains() because the issuer may also include a DID Fragment
-        if !self
-            .issuer
-            .to_string()
-            .contains(&bearer_did.did.uri.clone())
-        {
+        if self.issuer.to_string() != bearer_did.did.uri.clone() {
             return Err(Web5Error::Parameter(format!(
                 "bearer_did uri {} does not match issuer {}",
                 bearer_did.did.uri.clone(),
@@ -1464,6 +1460,129 @@ mod tests {
                 }
                 _ => panic!("Expected Web5Error::CredentialError, but got: {:?}", result),
             };
+        }
+    }
+
+    mod sign {
+        use super::*;
+        use crate::dids::methods::did_jwk::DidJwk;
+
+        lazy_static! {
+            static ref TEST_SUITE: UnitTestSuite =
+                UnitTestSuite::new("verifiable_credential_1_1_sign");
+        }
+
+        #[test]
+        fn z_assert_all_suite_cases_covered() {
+            // fn name prefixed with `z_*` b/c rust test harness executes in alphabetical order,
+            // unless intentionally executed with "shuffle" https://doc.rust-lang.org/rustc/tests/index.html#--shuffle
+            // this may not work if shuffled or if test list grows to the extent of 100ms being insufficient wait time
+
+            // wait 100ms to be last-in-queue of mutex lock
+            std::thread::sleep(std::time::Duration::from_millis(100));
+
+            TEST_SUITE.assert_coverage()
+        }
+
+        #[test]
+        fn test_can_sign_then_verify() {
+            TEST_SUITE.include(test_name!());
+
+            let bearer_did = DidJwk::create(None).unwrap();
+            let vc = VerifiableCredential::create(
+                Issuer::String(bearer_did.did.uri.clone()),
+                credential_subject(),
+                None,
+            )
+            .unwrap();
+
+            let key_id = bearer_did.document.verification_method[0].id.clone();
+            let signer = bearer_did.get_signer(&key_id).unwrap();
+            let vc_jwt = vc
+                .sign(&key_id, signer)
+                .expect("should be able to sign vc jwt");
+
+            let vc_from_vc_jwt = VerifiableCredential::from_vc_jwt(&vc_jwt, true)
+                .expect("should be able to verify the signed vc jwt");
+            assert_eq!(vc.id, vc_from_vc_jwt.id)
+        }
+    }
+
+    mod sign_with_did {
+        use crate::dids::methods::did_jwk::DidJwk;
+
+        use super::*;
+
+        lazy_static! {
+            static ref TEST_SUITE: UnitTestSuite =
+                UnitTestSuite::new("verifiable_credential_1_1_sign_with_did");
+        }
+
+        #[test]
+        fn z_assert_all_suite_cases_covered() {
+            // fn name prefixed with `z_*` b/c rust test harness executes in alphabetical order,
+            // unless intentionally executed with "shuffle" https://doc.rust-lang.org/rustc/tests/index.html#--shuffle
+            // this may not work if shuffled or if test list grows to the extent of 100ms being insufficient wait time
+
+            // wait 100ms to be last-in-queue of mutex lock
+            std::thread::sleep(std::time::Duration::from_millis(100));
+
+            TEST_SUITE.assert_coverage()
+        }
+
+        #[test]
+        fn test_bearer_did_mismatch_issuer() {
+            TEST_SUITE.include(test_name!());
+
+            let bearer_did = DidJwk::create(None).unwrap();
+            let vc = VerifiableCredential::create(
+                Issuer::String(bearer_did.did.uri.clone()),
+                credential_subject(),
+                None,
+            )
+            .unwrap();
+
+            let different_bearer_did = DidJwk::create(None).unwrap();
+            let result = vc.sign_with_did(&different_bearer_did, None);
+
+            match result {
+                Err(Web5Error::Parameter(err_msg)) => {
+                    assert_eq!(
+                        err_msg,
+                        format!(
+                            "bearer_did uri {} does not match issuer {}",
+                            different_bearer_did.did.uri, bearer_did.did.uri
+                        )
+                    )
+                }
+                _ => panic!("Expected Web5Error::Parameter, but got: {:?}", result),
+            };
+        }
+
+        #[test]
+        fn test_defaults_to_first_vm() {
+            TEST_SUITE.include(test_name!());
+
+            let bearer_did = DidJwk::create(None).unwrap();
+            let vc = VerifiableCredential::create(
+                Issuer::String(bearer_did.did.uri.clone()),
+                credential_subject(),
+                None,
+            )
+            .unwrap();
+
+            let vc_jwt = vc
+                .sign_with_did(&bearer_did, None)
+                .expect("should sign with default vm");
+
+            let header = josekit::jwt::decode_header(vc_jwt).unwrap();
+
+            let kid = header
+                .claim("kid")
+                .and_then(serde_json::Value::as_str)
+                .unwrap();
+
+            assert_eq!(bearer_did.document.verification_method[0].id, kid)
         }
     }
 }
