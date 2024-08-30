caacb9a38db2548e14f79165ca8c303c064ee616
Finn <139806439+finn-tbd@users.noreply.github.com>
2024-08-30 12:11:31 -0700

    Build CLI binaries and attach them to release (#330)
    Build CLI binaries and attach them to release
    
    Co-authored-by: Kendall Weihe <kendallweihe@gmail.com>
    
    ---------
    
    Co-authored-by: Kendall Weihe <kendallweihe@gmail.com>

--- crates/web5_cli/install.sh
+++ crates/web5_cli/install.sh
@@ -12,14 +12,14 @@ ARCH=$(uname -m)
 case $OS in
   "linux")
     case $ARCH in
-      "x86_64") FILENAME="web5-x86_64-linux-gnu.zip" ;;
+      "x86_64") FILENAME="web5-x86_64-linux-gnu" ;;
       *) echo "Unsupported architecture: $ARCH"; exit 1 ;;
     esac
     ;;
   "darwin")
     case $ARCH in
-      "x86_64") FILENAME="web5-x86_64-apple-darwin.zip" ;;
-      "arm64") FILENAME="web5-aarch64-apple-darwin.zip" ;;
+      "x86_64") FILENAME="web5-x86_64-apple-darwin" ;;
+      "arm64") FILENAME="web5-aarch64-apple-darwin" ;;
       *) echo "Unsupported architecture: $ARCH"; exit 1 ;;
     esac
     ;;
@@ -27,13 +27,14 @@ case $OS in
     echo "Unsupported OS: $OS"; exit 1 ;;
 esac
 
-# Download and unzip
+# Download
 curl -L -o /tmp/$FILENAME https://github.com/TBD54566975/web5-rs/releases/download/$VERSION/$FILENAME
-unzip -o /tmp/$FILENAME -d /tmp
 
-# Move the executable to /usr/local/bin and make it executable
-sudo mv /tmp/web5_cli /usr/local/bin/web5
-sudo chmod +x /usr/local/bin/web5
+# give it executable permissions
+chmod +x /tmp/$FILENAME
+
+# Move the executable to /usr/local/bin
+sudo mv /tmp/$FILENAME /usr/local/bin/web5
 
 # Cleanup
 rm /tmp/$FILENAME
