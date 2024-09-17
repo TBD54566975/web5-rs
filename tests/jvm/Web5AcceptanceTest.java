ad281638fd6696308b04d9276834b156c78fe758
Neal <groessler24@gmail.com>
2024-09-17 12:33:32 -0700

    update

---
+++ tests/jvm/Web5AcceptanceTest.java
@@ -0,0 +1,20 @@
+import web5.sdk.rust.SystemArchitecture;
+import web5.sdk.rust.UniffiLib;
+
+/**
+ * A simple main class to act as an acceptance smoke test for the Kotlin
+ * web5 binary.
+ *
+ * See README.md in this folder for usage and purpose.
+ */
+public class Web5AcceptanceTest {
+
+    public static void main(String... args) {
+        System.setProperty("WEB5_SDK_LOG_LEVEL", "debug");
+        SystemArchitecture.INSTANCE.set();
+        UniffiLib.Companion.getINSTANCE$web5();
+        System.out.println(
+                "Successfully loaded shared library for " +
+                        System.getProperty("uniffi.component.web5.libraryOverride"));
+    }
+}
