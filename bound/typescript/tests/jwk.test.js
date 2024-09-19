b21474582ab81df3392e2ac44d05400f33fa4191
Kendall Weihe <kendallw@squareup.com>
2024-09-19 17:05:16 -0400

    Add assertion to test

--- bound/typescript/tests/jwk.test.js
+++ bound/typescript/tests/jwk.test.js
@@ -1,7 +1,10 @@
 import web5 from "../dist/index.js";
-// import { expect } from 'chai'
+import { expect } from 'chai'
 
 describe("test EC valid", async () => {
   console.log(web5)
   console.log(web5.WasmJwk)
+  it('works', () => {
+    expect(1).to.equal(1)
+  })
 });
