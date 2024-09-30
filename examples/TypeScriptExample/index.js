6edd5894b2d28b201a33c6efc166960066258c83
Neal <groessler24@gmail.com>
2024-09-30 12:19:28 -0700

    update

--- examples/TypeScriptExample/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const web5_1 = require("web5");
-console.log(web5_1.dids);
--- examples/TypeScriptExample/index.ts
+++
@@ -1,2 +0,0 @@
-import { generate_ed25519_key } from '../../bound/typescript/dist/web5_wasm';
-console.log(generate_ed25519_key());
