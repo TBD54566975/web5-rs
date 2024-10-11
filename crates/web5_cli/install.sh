9d989efe1b8b94003f5f90cb0bb1d44c7d99a41d
Mohammed Mushahid Qureshi <mushahidq@gmail.com>
2024-10-11 16:17:30 +0530

    fixes #384 - fix for web cli installation script error

--- crates/web5_cli/install.sh
+++ crates/web5_cli/install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Clean up files in case of error
+clean_up_temp () {
+    [[ -f "/tmp/$FILENAME" ]] && rm -rf "/tmp/$temp_dir"
+}
+
+trap clean_up_temp EXIT
+
 if [ -z "$1" ]; then
   echo "Usage: $0 <version>"
   exit 1
@@ -28,12 +35,23 @@ case $OS in
 esac
 
 # Download
-curl -L -o /tmp/$FILENAME https://github.com/TBD54566975/web5-rs/releases/download/$VERSION/$FILENAME
+curl -L -f -o /tmp/$FILENAME https://github.com/TBD54566975/web5-rs/releases/download/$VERSION/$FILENAME
+
+# Check download errors
+if [ $? -ne 0 ] ; then
+  echo "Error while downloading $FILENAME"
+  echo "Exiting..."
+  exit 1
+fi
 
 # give it executable permissions
 chmod +x /tmp/$FILENAME
 
 # Move the executable to /usr/local/bin
+if [ -d "$DIRECTORY" ]; then
+  echo "Creating $DIRECTORY."
+  mkdir $DIRECTORY
+fi
 sudo mv /tmp/$FILENAME /usr/local/bin/web5
 
 # Cleanup
